08fe28bb5fb562a3efe3f9761b1303bbe2a327b0
Fetch Robotics <>
2023-04-05 15:55:19 -0400

    Removed unnecessary files and imports

--- src/keyboard_ikgoal_driver.py
+++ src/keyboard_ikgoal_driver.py
@@ -1,144 +1,230 @@
 #!/usr/bin/python
 
+import numpy as np
 import readchar
 import rospy
-from geometry_msgs.msg import PoseStamped, Vector3Stamped, QuaternionStamped, Pose
 from std_msgs.msg import Bool
 from relaxed_ik_ros1.msg import EEPoseGoals
 import transformations as T
+import kinova_positional_control.srv as posctrl_srv
+from positional_control import utils
 
-rospy.init_node('keyboard_ikgoal_driver')
 
-ik_goal_r_pub = rospy.Publisher('/ik_goal_r',PoseStamped,queue_size=5)
-ik_goal_l_pub = rospy.Publisher('/ik_goal_l',PoseStamped,queue_size=5)
-goal_pos_pub = rospy.Publisher('vive_position', Vector3Stamped)
-goal_quat_pub = rospy.Publisher('vive_quaternion', QuaternionStamped)
-ee_pose_goals_pub = rospy.Publisher('/relaxed_ik/ee_pose_goals', EEPoseGoals, queue_size=5)
-quit_pub = rospy.Publisher('/relaxed_ik/quit',Bool,queue_size=5)
+class KeyboardControl():
 
-pos_stride = 0.015
-rot_stride = 0.055
+    def __init__(self):
 
-position_r = [0,0,0]
-rotation_r = [1,0,0,0]
+        self.pos_stride = 0.015
+        self.rot_stride = 0.055
 
-position_l = [0,0,0]
-rotation_l = [1,0,0,0]
+        self.position_r = [0, 0, 0]
+        self.rotation_r = [1, 0, 0, 0]
 
-seq = 1
-rate = rospy.Rate(1000)
-while not rospy.is_shutdown():
+        self.position_l = [0, 0, 0]
+        self.rotation_l = [1, 0, 0, 0]
 
-    print("Pos R: {}, Pos L: {}".format(position_r, position_l))
+        #Publishers
+        self.ee_pose_goals_pub = rospy.Publisher(
+            '/relaxed_ik/ee_pose_goals',
+            EEPoseGoals,
+            queue_size=5,
+        )
+        self.quit_pub = rospy.Publisher(
+            '/relaxed_ik/quit',
+            Bool,
+            queue_size=5,
+        )
 
-    key = readchar.readkey()
-    if key == 'w':
-        position_r[0] += pos_stride
-    elif key == 'x':
-        position_r[0] -= pos_stride
-    elif key == 'a':
-        position_r[1] += pos_stride
-    elif key == 'd':
-        position_r[1] -= pos_stride
-    elif key == 'q':
-        position_r[2] += pos_stride
-    elif key == 'z':
-        position_r[2] -= pos_stride
-    elif key == '1':
-        euler = list(T.euler_from_quaternion(rotation_r))
-        euler[0] += rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '2':
-        euler = list(T.euler_from_quaternion(rotation_r))
-        euler[0] -= rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '3':
-        euler = list(T.euler_from_quaternion(rotation_r))
-        euler[1] += rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '4':
-        euler = list(T.euler_from_quaternion(rotation_r))
-        euler[1] -= rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '5':
-        euler = list(T.euler_from_quaternion(rotation_r))
-        euler[2] += rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '6':
-        euler = list(T.euler_from_quaternion(rotation_r))
-        euler[2] -= rot_stride
-        rotation_r = T.quaternion_from_euler(euler[0],euler[1],euler[2])
+        # Service
+        self.pid_vel_limit_srv = rospy.ServiceProxy(
+            'pid_vel_limit',
+            posctrl_srv.pid_vel_limit,
+        )
 
-    elif key == 'i':
-        position_l[0] += pos_stride # left
-    elif key == 'm':
-        position_l[0] -= pos_stride
-    elif key == 'j':
-        position_l[1] += pos_stride
-    elif key == 'l':
-        position_l[1] -= pos_stride
-    elif key == 'u':
-        position_l[2] += pos_stride
-    elif key == 'n':
-        position_l[2] -= pos_stride
-    elif key == '=':
-        euler = list(T.euler_from_quaternion(rotation_l))
-        euler[0] += rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '-':
-        euler = list(T.euler_from_quaternion(rotation_l))
-        euler[0] -= rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '0':
-        euler = list(T.euler_from_quaternion(rotation_l))
-        euler[1] += rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '9':
-        euler = list(T.euler_from_quaternion(rotation_l))
-        euler[1] -= rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '8':
-        euler = list(T.euler_from_quaternion(rotation_l))
-        euler[2] += rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == '7':
-        euler = list(T.euler_from_quaternion(rotation_l))
-        euler[2] -= rot_stride
-        rotation_l = T.quaternion_from_euler(euler[0],euler[1],euler[2])
-    elif key == 'q':
+        # Subscribers
+        rospy.Subscriber(
+            '/pid/motion_finished',
+            Bool,
+            self.pid_motion_finished_callback,
+        )
+
+    def pid_motion_finished_callback(self, data):
+        """Callback function for the '/pid/motion_finished' topic that updates the
+        'motionFinished' flag variable.
+        """
+
+        self.motionFinished = data.data
+
+    def wait_motion_finished(self):
+        """Block code execution until the 'motionFinished' flag is set or the ROS node
+        is shutdown.
+        """
+
+        # Allow motion to start
+        rospy.sleep(1)
+
+        # Block code execution
+        while not self.motionFinished and not rospy.is_shutdown():
+            pass
+
+    def initialization(self):
+        """Initializes the system by homing the robotic arm, with velocity limit
+        set to 20%, to a pre-specified initial configuration (starting_config in
+        kortex_info.yaml) with relaxedIK.
+        """
+
+        # Set 20% velocity
+        self.pid_vel_limit_srv(0.2)
+
+        # Homing position
+        print("\nHoming has started...\n")
+
+        # Let the node initialized
+        rospy.sleep(2)
+
+        # Home using relaxedIK
+        utils.relaxedik_publish([0, 0, 0], [1, 0, 0, 0])
+
+        # Block until the motion is finished
+        self.wait_motion_finished()
+
+        print("\nHoming has finished.\n")
+
+        self.pid_vel_limit_srv(1.0)
+
+        print("\nSystem is ready.\n")
+
+    def main(self):
+
+        print("Pos R: {}, Pos L: {}".format(self.position_r, self.position_l))
+
+        key = readchar.readkey()
+        if key == 'w':
+            self.position_r[0] += self.pos_stride
+        elif key == 'x':
+            self.position_r[0] -= self.pos_stride
+        elif key == 'a':
+            self.position_r[1] += self.pos_stride
+        elif key == 'd':
+            self.position_r[1] -= self.pos_stride
+        elif key == 'q':
+            self.position_r[2] += self.pos_stride
+        elif key == 'z':
+            self.position_r[2] -= self.pos_stride
+        elif key == '1':
+            euler = list(T.euler_from_quaternion(self.rotation_r))
+            euler[0] += self.rot_stride
+            self.rotation_r = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '2':
+            euler = list(T.euler_from_quaternion(self.rotation_r))
+            euler[0] -= self.rot_stride
+            self.rotation_r = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '3':
+            euler = list(T.euler_from_quaternion(self.rotation_r))
+            euler[1] += self.rot_stride
+            self.rotation_r = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '4':
+            euler = list(T.euler_from_quaternion(self.rotation_r))
+            euler[1] -= self.rot_stride
+            self.rotation_r = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '5':
+            euler = list(T.euler_from_quaternion(self.rotation_r))
+            euler[2] += self.rot_stride
+            self.rotation_r = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '6':
+            euler = list(T.euler_from_quaternion(self.rotation_r))
+            euler[2] -= self.rot_stride
+            self.rotation_r = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+
+        elif key == 'i':
+            self.position_l[0] += self.pos_stride  # left
+        elif key == 'm':
+            self.position_l[0] -= self.pos_stride
+        elif key == 'j':
+            self.position_l[1] += self.pos_stride
+        elif key == 'l':
+            self.position_l[1] -= self.pos_stride
+        elif key == 'u':
+            self.position_l[2] += self.pos_stride
+        elif key == 'n':
+            self.position_l[2] -= self.pos_stride
+        elif key == '=':
+            euler = list(T.euler_from_quaternion(self.rotation_l))
+            euler[0] += self.rot_stride
+            self.rotation_l = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '-':
+            euler = list(T.euler_from_quaternion(self.rotation_l))
+            euler[0] -= self.rot_stride
+            self.rotation_l = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '0':
+            euler = list(T.euler_from_quaternion(self.rotation_l))
+            euler[1] += self.rot_stride
+            self.rotation_l = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '9':
+            euler = list(T.euler_from_quaternion(self.rotation_l))
+            euler[1] -= self.rot_stride
+            self.rotation_l = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '8':
+            euler = list(T.euler_from_quaternion(self.rotation_l))
+            euler[2] += self.rot_stride
+            self.rotation_l = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == '7':
+            euler = list(T.euler_from_quaternion(self.rotation_l))
+            euler[2] -= self.rot_stride
+            self.rotation_l = T.quaternion_from_euler(
+                euler[0], euler[1], euler[2]
+            )
+        elif key == 'q':
+            q = Bool()
+            q.data = True
+            self.quit_pub.publish(q)
+        elif key == 'c':
+            rospy.signal_shutdown()
+
+        target_pos = np.array(
+            [self.position_r[0], self.position_r[1], self.position_r[2]]
+        )
+        target_orientation = np.array(
+            [
+                self.rotation_r[0], self.rotation_r[1], self.rotation_r[2],
+                self.rotation_r[3]
+            ]
+        )
+        utils.relaxedik_publish(target_pos, target_orientation)
+
         q = Bool()
-        q.data = True
-        quit_pub.publish(q)
-    elif key == 'c':
-        rospy.signal_shutdown()
+        q.data = False
+        self.quit_pub.publish(q)
 
-    ee_pose_goals = EEPoseGoals()
-    pose_r = Pose()
-    pose_r.position.x = position_r[0]
-    pose_r.position.y = position_r[1]
-    pose_r.position.z = position_r[2]
 
-    pose_r.orientation.w = rotation_r[0]
-    pose_r.orientation.x = rotation_r[1]
-    pose_r.orientation.y = rotation_r[2]
-    pose_r.orientation.z = rotation_r[3]
+if __name__ == '__main__':
 
-    pose_l = Pose()
-    pose_l.position.x = position_l[0]
-    pose_l.position.y = position_l[1]
-    pose_l.position.z = position_l[2]
+    rospy.init_node('keyboard_ikgoal_driver')
+    system = KeyboardControl()
 
-    pose_l.orientation.w = rotation_l[0]
-    pose_l.orientation.x = rotation_l[1]
-    pose_l.orientation.y = rotation_l[2]
-    pose_l.orientation.z = rotation_l[3]
-    ee_pose_goals.ee_poses.append(pose_r)
-    ee_pose_goals.ee_poses.append(pose_l)
+    system.initialization()
 
-    ee_pose_goals.header.seq = seq
-    seq += 1
-    ee_pose_goals_pub.publish(ee_pose_goals)
-
-    q = Bool()
-    q.data = False
-    quit_pub.publish(q)
+    while not rospy.is_shutdown():
+        system.main()
--- src/relaxed_ik_rust.py
+++ src/relaxed_ik_rust.py
@@ -1,37 +1,37 @@
 #! /usr/bin/env python
 
-import csv
 import ctypes
 import numpy
 import os
 import rospkg
 import rospy
-import sys
-import utils
-import transformations as T
 import yaml
 
 from relaxed_ik_ros1.msg import EEPoseGoals
 from relaxed_ik_ros1.srv import activate_ik
-from robot import Robot
-from std_msgs.msg import Float64, Bool
-from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64
 from timeit import default_timer as timer
-from urdf_load import urdf_load
 from visualization_msgs.msg import InteractiveMarkerFeedback, InteractiveMarkerUpdate
 
 from kortex_driver.msg import JointAngles, JointAngle
 
+
 class Opt(ctypes.Structure):
-    _fields_ = [("data", ctypes.POINTER(ctypes.c_double)), ("length", ctypes.c_int)]
+    _fields_ = [
+        ("data", ctypes.POINTER(ctypes.c_double)), ("length", ctypes.c_int)
+    ]
+
 
 path_to_src = rospkg.RosPack().get_path('relaxed_ik_ros1')
 env_settings_file_path = path_to_src + '/relaxed_ik_core/config/settings.yaml'
 
 os.chdir(path_to_src + "/relaxed_ik_core")
 
-lib = ctypes.cdll.LoadLibrary(path_to_src + '/relaxed_ik_core/target/debug/librelaxed_ik_lib.so')
+lib = ctypes.cdll.LoadLibrary(
+    path_to_src + '/relaxed_ik_core/target/debug/librelaxed_ik_lib.so'
+)
 lib.solve.restype = Opt
+
 
 def marker_feedback_cb(msg):
     pos_arr = (ctypes.c_double * 3)()
@@ -45,6 +45,7 @@ def marker_feedback_cb(msg):
     quat_arr[3] = msg.pose.orientation.w
     # Call the rust callback function
     lib.dynamic_obstacle_cb(msg.marker_name, pos_arr, quat_arr)
+
 
 def marker_update_cb(msg):
     # update dynamic collision obstacles in relaxed IK
@@ -61,10 +62,14 @@ def marker_update_cb(msg):
         # Call the rust callback function
         lib.dynamic_obstacle_cb(pose_stamped.name, pos_arr, quat_arr)
 
+
 eepg = None
+
+
 def eePoseGoals_cb(msg):
     global eepg
     eepg = msg
+
 
 # Turn relaxed IK calculation ON and OFF
 def activate_ik_handler(req):
@@ -73,6 +78,7 @@ def activate_ik_handler(req):
     activateIk = req.command
 
     return True
+
 
 def main(args=None):
     global activateIk
@@ -94,18 +100,24 @@ def main(args=None):
     info_file_name = robot_info['name']
     robot_name = info_file_name.split('_')[0]
     objective_mode = robot_info['objective_mode']
-    print("\nRelaxedIK initialized!\nRobot: {}\nObjective mode: {}\n".format(robot_name, objective_mode))
+    print(
+        "\nRelaxedIK initialized!\nRobot: {}\nObjective mode: {}\n".format(
+            robot_name, objective_mode
+        )
+    )
 
     # Publishers
-    angles_pub = rospy.Publisher('/relaxed_ik/joint_angle_solutions', JointAngles, queue_size=10)
-    time_pub = rospy.Publisher('/relaxed_ik/current_time', Float64, queue_size=10)
-
-    # Subscribers
-    rospy.Subscriber('/simple_marker/feedback', InteractiveMarkerFeedback, marker_feedback_cb)
-    rospy.Subscriber('/simple_marker/update', InteractiveMarkerUpdate, marker_update_cb)
+    angles_pub = rospy.Publisher(
+        '/relaxed_ik/joint_angle_solutions', JointAngles, queue_size=10
+    )
+    time_pub = rospy.Publisher(
+        '/relaxed_ik/current_time', Float64, queue_size=10
+    )
 
     # Services
-    activate_ik_srv = rospy.Service('/relaxed_ik/activate_ik', activate_ik, activate_ik_handler)
+    activate_ik_srv = rospy.Service(
+        '/relaxed_ik/activate_ik', activate_ik, activate_ik_handler
+    )
 
     cur_time = 0.0
     delta_time = 0.01
@@ -115,7 +127,8 @@ def main(args=None):
 
     rospy.Subscriber('/relaxed_ik/ee_pose_goals', EEPoseGoals, eePoseGoals_cb)
 
-    while eepg == None: continue
+    while eepg == None:
+        continue
 
     rate = rospy.Rate(3000)
     speed_list = []
@@ -126,26 +139,24 @@ def main(args=None):
         cur_time += delta_time * step
 
         pose_goals = eepg.ee_poses
-        header = eepg.header
         pos_arr = (ctypes.c_double * (3 * len(pose_goals)))()
         quat_arr = (ctypes.c_double * (4 * len(pose_goals)))()
 
         for i in range(len(pose_goals)):
             p = pose_goals[i]
-            pos_arr[3*i] = p.position.x
-            pos_arr[3*i+1] = p.position.y
-            pos_arr[3*i+2] = p.position.z
+            pos_arr[3 * i] = p.position.x
+            pos_arr[3 * i + 1] = p.position.y
+            pos_arr[3 * i + 2] = p.position.z
 
-            quat_arr[4*i] = p.orientation.x
-            quat_arr[4*i+1] = p.orientation.y
-            quat_arr[4*i+2] = p.orientation.z
-            quat_arr[4*i+3] = p.orientation.w
+            quat_arr[4 * i] = p.orientation.x
+            quat_arr[4 * i + 1] = p.orientation.y
+            quat_arr[4 * i + 2] = p.orientation.z
+            quat_arr[4 * i + 3] = p.orientation.w
 
         start = timer()
         xopt = lib.solve(pos_arr, len(pos_arr), quat_arr, len(quat_arr))
         end = timer()
         speed = 1.0 / (end - start)
-        #print("Speed: {}".format(speed))
         speed_list.append(speed)
 
         ja = JointAngles()
@@ -171,6 +182,7 @@ def main(args=None):
     print("Average speed: {} HZ".format(numpy.mean(speed_list)))
     print("Min speed: {} HZ".format(numpy.min(speed_list)))
     print("Max speed: {} HZ".format(numpy.max(speed_list)))
+
 
 if __name__ == '__main__':
     main()
